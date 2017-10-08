e74b026928f88ea1ab5915b63ba00df24e0d3d8c
Nicolás Alliaume <nicolas@on-lab.com>
2017-10-07 23:33:13 -0400

    ++ billing services to projects

--- api/projects.js
+++ api/projects.js
@@ -1,4 +1,8 @@
-var ProjectModel = require('./../models/project');
+const ProjectModel = require('./../models/project');
+const WorkEntryModel = require('./../models/work_entry');
+const TaskModel = require('./../models/task');
+const ObjectiveModel = require('./../models/objective');
+const moment = require('moment');
 
 /*
 	POST	/api/{v}/projects/add
@@ -6,11 +10,14 @@ var ProjectModel = require('./../models/project');
 	POST	/api/{v}/projects/:id
 
 	GET		/api/{v}/projects
+
+	GET 	/api/{v}/projects/billing
  */
 exports.setup = (router) => {
+	router.get('/projects', exports.getProjects);
+	router.get('/projects/billing', exports.getProjectsBilling);
 	router.post('/projects/add', exports.createProject);
 	router.post('/projects/:projectId', exports.updateProject);
-	router.get('/projects', exports.getProjects);
 }
 
 exports.createProject = function(req, res) {
@@ -33,11 +40,139 @@ exports.updateProject = function(req, res) {
 }
 
 exports.getProjects = function(req, res) {
-	ProjectModel.find().sort({ name : 1 })
+	ProjectModel.find({active: true}, {invoices: 0}).sort({name: 1})
 		.then((projects) => {
 			res.json({ projects })
 		})
 		.catch((e) => {
 			res.json({ error: e.message })
 		})
+}
+
+exports.getProjectsBilling = function(req, res) {
+	ProjectModel.find({active: true}).sort({name: 1}).lean()
+		.then(projects => exports.calculateBillingVariables(projects))
+		.then(projects => { res.json(projects) })
+		.catch((e) => {
+			res.json({ error: e.message })
+		})
+}
+
+/**
+ * Here we calculate the following variables for each project
+ * and resolve the promise with the variabled added to each
+ * project object:
+ * 
+ * 	- amount billed this month
+ * 	- amount billed total
+ * 	- hours registered this month
+ * 	- hours registered total
+ * 	
+ * @param  {Array} projects 
+ * @return {Promise}
+ */
+exports.calculateBillingVariables = function(projects) {
+	return new Promise((resolve, reject) => {
+		// calculate hours executed for this month and total
+		const promises = exports.calculateExecutedHoursForProjects(projects);
+		// after calculating executed hours, we calculate 
+		// billed hours, merge all that data into the projects
+		// and resolve this function's promise
+		Promise.all(promises).then(hours => {
+			const result = projects.map((p, idx) => {
+				return Object.assign({}, p, hours[idx], {
+					billed_hours_month  : exports.calculateThisMonthBillingHours(p),
+					billed_amount_month : exports.calculateThisMonthBillingAmount(p),
+					billed_hours_total  : exports.calculateTotalBillingHours(p),
+					billed_amount_total : exports.calculateTotalBillingAmount(p)
+				})
+			})
+			return resolve(result);
+		})
+	})
+}
+
+exports.calculateExecutedHoursForProjects = function(projects) {
+	const promises = projects.map(p => {
+		return Promise.all([
+			exports.calculateThisMonthExecuted(p._id),
+			exports.calculateTotalExecuted(p._id)
+		]).then(([month, total]) => {
+			return {
+				executed_hours_month : month,
+				executed_hours_total : total
+			}
+		})
+	})
+	return promises;
+}
+
+exports.calculateTotalExecuted = function(projectId) {
+	return getWorkEntriesForProject(projectId)
+		.then(workEntries => reduceWorkEntries(workEntries))
+}
+
+exports.calculateThisMonthExecuted = function(projectId) {
+	return getWorkEntriesForProject(projectId, getFilterForMonth(new Date()))
+		.then(workEntries => reduceWorkEntries(workEntries))
+}
+
+const reduceWorkEntries = function(workEntries) {
+	return workEntries.map(we => we.time).reduce((total, t) => t+total, 0);
+}
+
+function getWorkEntriesForProject(projectId, filters = {}) {
+	return getTasksForProject(projectId)
+		.then(taskIds => getObjectivesForTasks(taskIds))
+		.then(objectiveIds => getWorkEntriesForObjectives(objectiveIds, filters))
+}
+
+function getWorkEntriesForObjectives(objectiveIds, filters) {
+	const query = Object.assign(filters, {objective: {$in: objectiveIds}});
+	return WorkEntryModel.find(query)
+		.lean();
+}
+
+function getObjectivesForTasks(taskIds) {
+	return ObjectiveModel.find({related_task: {$in: taskIds}})
+		.lean().then(docs => docs.map(d => d._id));
+}
+
+function getTasksForProject(projectId) {
+	return TaskModel.find({project: projectId})
+		.lean().then(docs => docs.map(d => d._id));
+}
+
+exports.calculateThisMonthBillingHours = function(project) {
+	return exports.reduceInvoicesFieldWithCondition('billed_hours', 
+		i => isThisMonth(i.invoicing_date), project.invoices);
+}
+
+exports.calculateThisMonthBillingAmount = function(project) {
+	return exports.reduceInvoicesFieldWithCondition('amount', 
+		i => isThisMonth(i.invoicing_date), project.invoices);
+}
+
+exports.calculateTotalBillingHours = function(project) {
+	return exports.reduceInvoicesFieldWithCondition('billed_hours', 
+		i => true, project.invoices);
+}
+
+exports.calculateTotalBillingAmount = function(project) {
+	return exports.reduceInvoicesFieldWithCondition('amount', 
+		i => true, project.invoices);
+}
+
+exports.reduceInvoicesFieldWithCondition = function(field, condition, invoices) {
+	return sum(invoices.filter(condition).map(i => i[field]));
+}
+
+const isThisMonth = (date) => moment.utc(date).format('YYYY/MM') === moment.utc().format('YYYY/MM');
+
+const sum = (numbers) => numbers.reduce((total, n) => n+total, 0);
+
+const getFilterForMonth = (date) => {
+	const fromDate = moment.utc(date).startOf('month').startOf('day').toDate();
+	const toDate = moment.utc(date).endOf('month').endOf('day').toDate();
+	return { created_ts: {$gte: fromDate, $lte: toDate} }
 }
--- models/project.js
+++ models/project.js
@@ -1,12 +1,25 @@
 var mongoose = require ('mongoose');
 var Schema = mongoose.Schema;
 
+var invoiceSchema = new Schema({
+	description 	  	: { type : String, required : true },
+	amount 				: { type : Number, required : true },
+	billed_hours 		: { type : Number, required : true },
+	invoicing_date     	: { type : Date, required : true },
+	project 		  	: { type : mongoose.Schema.Types.ObjectId, ref : 'Project', require : true },
+	paid 				: { type : Boolean, default : false },
+
+	created_ts 		  	: { type : Date, default : Date.now },
+	created_by 		  	: { type : mongoose.Schema.Types.ObjectId, ref : 'User', required : true }
+});
+
 var schema = new Schema({
 	name 			: { type : String, required : true },
 	hours_sold		: { type : Number, min : 0, required : true },
 	hours_sold_unit : { type : String, enum : ['monthly', 'total'], lowercase : true, default : 'total' },
 	hourly_rate		: { type : Number, required : true, min : 0 },
-
+	active 			: { type : Boolean, default : true },
+	invoices 		: { type : [invoiceSchema], default : [] },
 	created_ts		: { type : Date, default : Date.now }
 });
 
