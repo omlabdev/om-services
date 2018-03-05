0d47e9e808d7aa0f33e77ce8aabe539bd6f2241a
Nicolás Alliaume <nicolas@on-lab.com>
2018-03-05 10:19:18 -0500

    Building and pushing to heroku

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-echo "Commiting new changes.."
+echo "==> [STEP] Setting Heroku account to personal.."
+heroku accounts:set personal
+echo "==> [STEP] Building project.."
+npm run build
+echo "==> [STEP] Commiting new changes.."
 git add . 
 git commit -m "Building and pushing to heroku"
-echo "Pushing to origin.."
+echo "==> [STEP] Pushing to origin.."
 git push origin
-echo "Pushing to heroku dev:master.."
+echo "==> [STEP] Pushing to heroku dev:master.."
 git push heroku dev:master
-echo "Done!"
+echo "==> [STEP] Done!"
